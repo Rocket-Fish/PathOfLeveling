c39650c09cdb7a4fba931fcaad5663870dca550c
Rocket Fish <rocket.fish@Mail.com>
2017-06-20 07:34:09 -0400

    Revered Using the Json c++ api... cannot parse the POE json files properly

--- Path Of Leveling/Path Of Leveling/ButtonFunctions.cpp
+++ Path Of Leveling/Path Of Leveling/ButtonFunctions.cpp
@@ -2,110 +2,99 @@
 #include "ButtonFunctions.h"
 #include "NetworkRequests.h"
 #include <cstdlib>
-#include "json.h"
-using json = nlohmann::json;
 
-	WNDPROC  charButtonProc;
-	WNDPROC  gearButtonProc;
-	WNDPROC  treeButtonProc;
 
-	HINSTANCE hInstanceForButton;
-	HWND hWnd_parent_forButton;
-	HWND currentDisplay_HWND; 
-	void setHInstForOutput(HINSTANCE hInstt) {
-		hInstanceForButton = hInstt;
+WNDPROC  charButtonProc;
+WNDPROC  gearButtonProc;
+WNDPROC  treeButtonProc;
 
-	}
+HINSTANCE hInstanceForButton;
+HWND hWnd_parent_forButton;
+HWND currentDisplay_HWND; 
+void setHInstForOutput(HINSTANCE hInstt) {
+	hInstanceForButton = hInstt;
 
-	LPWSTR chartoLPWSTR(const char* cs) {
-	size_t wn = mbsrtowcs(NULL, &cs, 0, NULL);
-	// error if wn == size_t(-1)
-	wchar_t * buf = new wchar_t[wn + 1]();  // value-initialize to 0 (see below)
-	wn = mbsrtowcs(buf, &cs, wn + 1, NULL);
-	// error if wn == size_t(-1)
-	//			assert(cs == NULL); // successful conversion
-	// result now in buf, return e.g. as std::wstring
-	return buf;
-	}
+}
 
-	LRESULT CALLBACK CharButtonProc(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp)
-	{
-		switch (msg) {
-		case WM_LBUTTONUP:
-			char * cs = ReadCharFrom("https://www.pathofexile.com/character-window/get-characters?accountName=RockyFish");
+LPWSTR chartoLPWSTR(const char* cs) {
+size_t wn = mbsrtowcs(NULL, &cs, 0, NULL);
+// error if wn == size_t(-1)
+wchar_t * buf = new wchar_t[wn + 1]();  // value-initialize to 0 (see below)
+wn = mbsrtowcs(buf, &cs, wn + 1, NULL);
+// error if wn == size_t(-1)
+//			assert(cs == NULL); // successful conversion
+// result now in buf, return e.g. as std::wstring
+return buf;
+}
 
-			////// Deconstruct the Json char array
-			json j = json::parse(cs);
-			std::string s = j.dump(4);
-			const char * ss = s.c_str();
-			LPWSTR str = chartoLPWSTR(ss);
+LRESULT CALLBACK CharButtonProc(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp)
+{
+	switch (msg) {
+	case WM_LBUTTONUP:
+		char * cs = ReadCharFrom("https://www.pathofexile.com/character-window/get-characters?accountName=RockyFish");
 
-			if (currentDisplay_HWND != NULL) {
-				DestroyWindow(currentDisplay_HWND);
-			}
-			currentDisplay_HWND = CreateWindow(L"STATIC", str, WS_VISIBLE | WS_CHILD | SS_LEFT, 10, 60, 850, 500, hWnd_parent_forButton, NULL, hInstanceForButton, NULL);
-			delete(str);
-			delete(cs);
-			break;
+		LPWSTR str = chartoLPWSTR(cs);
+
+		if (currentDisplay_HWND != NULL) {
+			DestroyWindow(currentDisplay_HWND);
 		}
-		return CallWindowProc(charButtonProc, hwnd, msg, wp, lp);
+		currentDisplay_HWND = CreateWindow(L"STATIC", str, WS_VISIBLE | WS_CHILD | SS_LEFT, 10, 60, 850, 500, hWnd_parent_forButton, NULL, hInstanceForButton, NULL);
+		delete(str);
+		delete(cs);
+		break;
 	}
+	return CallWindowProc(charButtonProc, hwnd, msg, wp, lp);
+}
 
-	LRESULT CALLBACK GearButtonProc(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp)
-	{
-		switch (msg) {
-		case WM_LBUTTONUP:
-			char * cs = ReadCharFrom("https://www.pathofexile.com/character-window/get-items?character=AWanderingFish&accountName=RockyFish");
-			////// Deconstruct the Json char array
-			json j = json::parse(cs);
-			std::string s = j.dump(4);
-			const char * ss = s.c_str();
-			LPWSTR str = chartoLPWSTR(ss);
+LRESULT CALLBACK GearButtonProc(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp)
+{
+	switch (msg) {
+	case WM_LBUTTONUP:
+		char * cs = ReadCharFrom("https://www.pathofexile.com/character-window/get-items?character=AWanderingFish&accountName=RockyFish");
+		////// Deconstruct the Json char array
+		LPWSTR str = chartoLPWSTR(cs);
 
-			if (currentDisplay_HWND != NULL) {
-				DestroyWindow(currentDisplay_HWND);
-			}
-			currentDisplay_HWND = CreateWindow(L"STATIC", str, WS_VISIBLE | WS_CHILD | SS_LEFT, 10, 60, 850, 500, hWnd_parent_forButton, NULL, hInstanceForButton, NULL);
-			delete(str);
-			delete(cs);
-			break;
+		if (currentDisplay_HWND != NULL) {
+			DestroyWindow(currentDisplay_HWND);
 		}
-		return CallWindowProc(gearButtonProc, hwnd, msg, wp, lp);
+		currentDisplay_HWND = CreateWindow(L"STATIC", str, WS_VISIBLE | WS_CHILD | SS_LEFT, 10, 60, 850, 500, hWnd_parent_forButton, NULL, hInstanceForButton, NULL);
+		delete(str);
+		delete(cs);
+		break;
 	}
+	return CallWindowProc(gearButtonProc, hwnd, msg, wp, lp);
+}
 
-	LRESULT CALLBACK TreeButtonProc(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp)
-	{
-		switch (msg) {
-		case WM_LBUTTONUP:
-			char * cs = ReadCharFrom("https://www.pathofexile.com/character-window/get-passive-skills?character=AWanderingFish&accountName=RockyFish");
-			////// Deconstruct the Json char array
-			json j = json::parse(cs);
-			std::string s = j.dump(4);
-			const char * ss = s.c_str();
-			LPWSTR str = chartoLPWSTR(ss);
+LRESULT CALLBACK TreeButtonProc(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp)
+{
+	switch (msg) {
+	case WM_LBUTTONUP:
+		char * cs = ReadCharFrom("https://www.pathofexile.com/character-window/get-passive-skills?character=AWanderingFish&accountName=RockyFish");
+		////// Deconstruct the Json char array
+		LPWSTR str = chartoLPWSTR(cs);
 
-			if (currentDisplay_HWND != NULL) {
-				DestroyWindow(currentDisplay_HWND);
-			}
-			currentDisplay_HWND = CreateWindow(L"STATIC", str, WS_VISIBLE | WS_CHILD | SS_LEFT, 10, 60, 850, 500, hWnd_parent_forButton, NULL, hInstanceForButton, NULL);
-			delete(str);
-			delete(cs);
-			break;
+		if (currentDisplay_HWND != NULL) {
+			DestroyWindow(currentDisplay_HWND);
 		}
-		return CallWindowProc(treeButtonProc, hwnd, msg, wp, lp);
+		currentDisplay_HWND = CreateWindow(L"STATIC", str, WS_VISIBLE | WS_CHILD | SS_LEFT, 10, 60, 850, 500, hWnd_parent_forButton, NULL, hInstanceForButton, NULL);
+		delete(str);
+		delete(cs);
+		break;
 	}
+	return CallWindowProc(treeButtonProc, hwnd, msg, wp, lp);
+}
 
 
-	void MakeTheButtons(HWND hWnd_parent) {
-		hWnd_parent_forButton = hWnd_parent;
+void MakeTheButtons(HWND hWnd_parent) {
+	hWnd_parent_forButton = hWnd_parent;
 
-		HWND charButton = CreateButton(hWnd_parent, L"Get Char List", 10, 10);
-		HWND gearButton = CreateButton(hWnd_parent, L"Get Gear", 160, 10);
-		HWND treeButton = CreateButton(hWnd_parent, L"Get Tree", 310, 10);
-		charButtonProc = (WNDPROC)
-			SetWindowLong(charButton, GWL_WNDPROC, (LONG)CharButtonProc);
-		gearButtonProc = (WNDPROC)
-			SetWindowLong(gearButton, GWL_WNDPROC, (LONG)GearButtonProc);
-		treeButtonProc = (WNDPROC)
-			SetWindowLong(treeButton, GWL_WNDPROC, (LONG)TreeButtonProc);
-	}
+	HWND charButton = CreateButton(hWnd_parent, L"Get Char List", 10, 10);
+	HWND gearButton = CreateButton(hWnd_parent, L"Get Gear", 160, 10);
+	HWND treeButton = CreateButton(hWnd_parent, L"Get Tree", 310, 10);
+	charButtonProc = (WNDPROC)
+		SetWindowLong(charButton, GWL_WNDPROC, (LONG)CharButtonProc);
+	gearButtonProc = (WNDPROC)
+		SetWindowLong(gearButton, GWL_WNDPROC, (LONG)GearButtonProc);
+	treeButtonProc = (WNDPROC)
+		SetWindowLong(treeButton, GWL_WNDPROC, (LONG)TreeButtonProc);
+}
--- Path Of Leveling/Path Of Leveling/ButtonFunctions.h
+++ Path Of Leveling/Path Of Leveling/ButtonFunctions.h
@@ -1,3 +1,4 @@
 #pragma once
+#include <string>
 void setHInstForOutput(HINSTANCE hInstt);
 void MakeTheButtons(HWND hWnd_parent);
